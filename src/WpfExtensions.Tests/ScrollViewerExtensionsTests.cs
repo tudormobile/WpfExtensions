cd0c69af800c569761aa08016138987c6e571f83
Bill Tudor <bill@tudormobile.com>
2025-01-17 13:56:48 -0500

    Added extensions for the ScrollViewer.

---
+++ src/WpfExtensions.Tests/ScrollViewerExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Controls;
+using Tudormobile.WpfExtensions;
+
+namespace WpfExtensions.Tests;
+
+[STATestClass]
+public class ScrollViewerExtensionsTests
+{
+    [TestMethod]
+    public void BothScrollBarsVisibleTest()
+    {
+        var expected = ScrollBarVisibility.Visible;
+        var target = new ScrollViewer();
+        Assert.AreSame(target, target.BothScrollBarsVisible());
+        Assert.AreEqual(expected, target.HorizontalScrollBarVisibility);
+        Assert.AreEqual(expected, target.VerticalScrollBarVisibility);
+    }
+
+    [TestMethod]
+    public void HorizontalScrollBarVisibleTest()
+    {
+        var expected = ScrollBarVisibility.Visible;
+        var target = new ScrollViewer();
+        Assert.AreSame(target, target.HorizontalScrollBarVisible());
+        Assert.AreEqual(expected, target.HorizontalScrollBarVisibility);
+        Assert.AreEqual(expected, target.VerticalScrollBarVisibility);
+    }
+
+    [TestMethod]
+    public void ScrollBarsVisibilityTest()
+    {
+        var target = new ScrollViewer();
+        Assert.AreSame(target, target.ScrollBarsVisibility(horizontalVisibility: ScrollBarVisibility.Auto));
+        Assert.AreEqual(ScrollBarVisibility.Auto, target.HorizontalScrollBarVisibility);
+        Assert.AreEqual(ScrollBarVisibility.Visible, target.VerticalScrollBarVisibility);
+    }
+}
---
+++ src/WpfExtensions/ScrollViewerExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Controls;
+
+namespace Tudormobile.WpfExtensions;
+
+/// <summary>
+/// Extensions for ScrollViewer.
+/// </summary>
+public static class ScrollViewerExtensions
+{
+    /// <summary>
+    /// Make both scroll bars visible.
+    /// </summary>
+    /// <param name="scrollViewer">ScrollViewer to extend.</param>
+    /// <returns>The extended scroll viewer.</returns>
+    public static ScrollViewer BothScrollBarsVisible(this ScrollViewer scrollViewer)
+    {
+        scrollViewer.HorizontalScrollBarVisibility = ScrollBarVisibility.Visible;
+        scrollViewer.VerticalScrollBarVisibility = ScrollBarVisibility.Visible;
+        return scrollViewer;
+    }
+
+    /// <summary>
+    /// Make the horizontal scroll bar visible.
+    /// </summary>
+    /// <param name="scrollViewer">ScrollViewer to extend.</param>
+    /// <returns>The extended scroll viewer.</returns>
+    public static ScrollViewer HorizontalScrollBarVisible(this ScrollViewer scrollViewer)
+    {
+        scrollViewer.HorizontalScrollBarVisibility = ScrollBarVisibility.Visible;
+        return scrollViewer;
+    }
+
+    /// <summary>
+    /// Set the visibility of the scroll bars.
+    /// </summary>
+    /// <param name="scrollViewer">ScrollViewer to extend.</param>
+    /// <param name="horizontalVisibility">Horizontal scrollbar visibility.</param>
+    /// <param name="verticalScrollBarVisibility">OPTIONAL; vertical scrollbar visiblity (default = Visible).</param>
+    /// <returns>The extended scroll viewer.</returns>
+    /// <remarks>
+    /// This method can be used to set both the horizontal and vertical scroll bar visibility. If only the horizontal
+    /// parameter is provided, the vertical scrollbar is set to Visible.
+    /// </remarks>
+    public static ScrollViewer ScrollBarsVisibility(this ScrollViewer scrollViewer, ScrollBarVisibility horizontalVisibility, ScrollBarVisibility verticalScrollBarVisibility = ScrollBarVisibility.Visible)
+    {
+        scrollViewer.HorizontalScrollBarVisibility = horizontalVisibility;
+        scrollViewer.VerticalScrollBarVisibility = verticalScrollBarVisibility;
+        return scrollViewer;
+    }
+}
