78f18cfe4fe52f58365ccb27e2818aa5940d836d
Bill Tudor <bill@tudormobile.com>
2025-01-18 16:17:35 -0500

    Additional Unit Tests.

--- src/WpfExtensions.Tests/HeaderedItemsControlExtensionsTests.cs
+++ src/WpfExtensions.Tests/HeaderedItemsControlExtensionsTests.cs
@@ -1,19 +1,31 @@
 ﻿using System.Windows.Controls;
 using Tudormobile.WpfExtensions;
 
-namespace WpfExtensions.Tests
+namespace WpfExtensions.Tests;
+
+[STATestClass]
+public class HeaderedItemsControlExtensionsTests
 {
-    [STATestClass]
-    public class HeaderedItemsControlExtensionsTests
+    [TestMethod]
+    public void HeaderTest()
     {
-        [TestMethod]
-        public void HeaderTest()
-        {
-            var expected = "This is a test";
-            var target = new MenuItem();
-            Assert.AreEqual(target, target.Header(expected));
-            var actual = target.Header;
-            Assert.AreEqual(expected, actual);
-        }
+        var expected = "This is a test";
+        var target = new MenuItem();
+        Assert.AreEqual(target, target.Header(expected));
+        var actual = target.Header;
+        Assert.AreEqual(expected, actual);
     }
+
+    [TestMethod]
+    public void HeaderTest1()
+    {
+        var expected = DateTime.Now;
+        var format = "D";
+        var target = new MenuItem();
+        Assert.AreSame(target, target.Header(expected, format));
+        var actual = target.Header;
+        Assert.AreEqual(expected, actual);
+        Assert.AreEqual(format, target.HeaderStringFormat);
+    }
+
 }
